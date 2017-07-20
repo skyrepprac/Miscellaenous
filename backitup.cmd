f9c248fed291f723750014694bc8f9faf538ff32
skyrepprac <viveks@e2msol.com>
2017-07-21 00:54:14 +0530

    Create backitup.cmd

---
+++ backitup.cmd
@@ -0,0 +1,11 @@
+@echo on
+for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set dt=%%a
+set YYYY=%dt:~0,4%
+set MM=%dt:~4,2%
+set DD=%dt:~6,2%
+set HH=%dt:~8,2%
+set Min=%dt:~10,2%
+set Sec=%dt:~12,2%
+set stamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%
+copy "<filename>" "%CD%\<filename>_%stamp%"
+pause
