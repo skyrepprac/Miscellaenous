c37fbfba20a15dca7cd51a10cf8ab5cdfc28cf3b
skyrepprac <viveks@e2msol.com>
2017-08-13 22:40:09 +0530

    Update backitup.cmd

--- backitup.cmd
+++ backitup.cmd
@@ -1,5 +1,6 @@
-@echo on
+@echo off
 for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set dt=%%a
+set /p filename="File to backup: "
 set YYYY=%dt:~0,4%
 set MM=%dt:~4,2%
 set DD=%dt:~6,2%
@@ -7,5 +8,6 @@ set HH=%dt:~8,2%
 set Min=%dt:~10,2%
 set Sec=%dt:~12,2%
 set stamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%
-copy "<filename>" "%CD%\<filename>_%stamp%"
+copy "%filename%" "%CD%\%filename%_%stamp%"
 pause
+exit
