272ee377d7da21b394b6a7fe999a9a710117371a
skyrepprac <viveks@e2msol.com>
2017-08-28 21:11:12 +0530

    Add files via upload

---
+++ bakallup_filetype.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set dt=%%a
+set YYYY=%dt:~0,4%
+set MM=%dt:~4,2%
+set DD=%dt:~6,2%
+set HH=%dt:~8,2%
+set Min=%dt:~10,2%
+set Sec=%dt:~12,2%
+set stamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%
+
+robocopy "%cd%" "%cd%\__bakfiles\%stamp%" *.php *.phtml *.xml *.css *.js *.bat *.cmd *.sublime-snippet
+echo "All files of directory %cd% were copied to %cd%\__bakfiles\%stamp% !!!"
+pause > nul
